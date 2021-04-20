a3bd4c8b8bcce96d8ecfc3ce12363181833dfe8a
DrewMcB <drew.mcbride89@gmail.com>
2021-04-19 20:55:42 -0500

    Added code for connecting lat/lng

--- static/js/logic.js
+++ static/js/logic.js
@@ -8,3 +8,17 @@ var myMap = L.map("mapid", {
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(myMap);
+
+  d3.json("  https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+    //   console.log(data);
+    L.geoJSON(data, {
+        pointTolayer: function(feature, latlng) {
+            return L.circleMarker(latlng)
+        },
+        onEachfeature: function(feature, layer){
+            layer.bindPopup (
+                "Magnitude: " +feature.properties.mag+ " Depth: "+feature.geometry.coordinates[2]+ "Location: " +feature.properties.place
+            )
+        }
+    }).addTo(myMap);
+});
