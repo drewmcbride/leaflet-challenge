185588ce43eb46316698994a59b6a86166e95ae3
DrewMcB <drew.mcbride89@gmail.com>
2021-04-19 22:11:11 -0500

    Fixed style.css

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,20 +1,26 @@
 // Create a map object.
 var myMap = L.map("mapid", {
-    center: [40.7607, -111.8910],
-    zoom: 3
+    center: [40.7607, -111.89105],
+    zoom: 4
   });
   
-  // Add a tile layer.
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(myMap);
+// Add a tile layer.
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
 
-  d3.json("  https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+var baseUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+d3.json(baseUrl).then(function(data) {
     //   console.log(data);
+    var earthquake = data.features
+    console.log(earthquake[0])
     L.geoJSON(data, {
-        pointTolayer: function(feature, latlng) {
-            return L.circleMarker(latlng)
+        pointTolayer: function(feature, latLng){
+            console.log(feature);
+            return L.circleMarker(latLng)
         },
+        
         onEachfeature: function(feature, layer){
             layer.bindPopup (
                 "Magnitude: " +feature.properties.mag+ " Depth: "+feature.geometry.coordinates[2]+ "Location: " +feature.properties.place
