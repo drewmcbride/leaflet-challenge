9641c76e2101b238cc3f032e919c8c8b9988cc32
DrewMcB <drew.mcbride89@gmail.com>
2021-04-19 20:34:02 -0500

    Added initial map code

---
+++ static/js/logic.js
@@ -0,0 +1,10 @@
+// Create a map object.
+var myMap = L.map("mapid", {
+    center: [40.7607, -111.8910],
+    zoom: 3
+  });
+  
+  // Add a tile layer.
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(myMap);
